a9aed79ac829d35cbc824dd36c90f17668449550
unknown <yevheniikyvenko@gmail.com>
2020-06-30 22:32:44 +0300

    Updates

---
+++ precache-manifest.0df33861db68822c3e2c0246b9b85b40.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4823017e5a53c6d8d4fb4b4e89a27cd5",
+    "url": "/react-social/index.html"
+  },
+  {
+    "revision": "c8234e9bfae8b7478030",
+    "url": "/react-social/static/css/main.1f11db91.chunk.css"
+  },
+  {
+    "revision": "c2ab0adf67ff4bd72b05",
+    "url": "/react-social/static/js/2.e9af92f3.chunk.js"
+  },
+  {
+    "revision": "c8234e9bfae8b7478030",
+    "url": "/react-social/static/js/main.08847ce1.chunk.js"
+  },
+  {
+    "revision": "66144349c72431ab8dd5",
+    "url": "/react-social/static/js/runtime~main.90a5cb61.js"
+  },
+  {
+    "revision": "11fb1b6be461f165ff323b715996533d",
+    "url": "/react-social/static/media/2.11fb1b6b.svg"
+  },
+  {
+    "revision": "df1bbd844af9c74a14092d63316b01b4",
+    "url": "/react-social/static/media/bg-image.df1bbd84.jpg"
+  },
+  {
+    "revision": "d96739352132fdde8c78b014286a981e",
+    "url": "/react-social/static/media/user-pic.d9673935.png"
+  }
+]);
